988f553f00e0c6060e489912dae13f5697f87cb2
Jason Wuerch <wuerchjason@gmail.com>
2016-08-09 14:37:24 -0700

    make app print out the rgba values of the color blue

---
+++ Block.elm
@@ -0,0 +1,20 @@
+module Block exposing (..)
+
+import Basics exposing (..)
+import Color exposing (Color)
+import Collage exposing (..)
+import Html exposing (..)
+import Element exposing (..)
+
+type alias Block = { color: Color }
+
+size: Float
+size = 25
+
+toForm : Block -> Form
+toForm block =
+    let shape = square size
+    in filled block.color shape
+
+main =
+    toHtml <| show (Block Color.blue)
--- Hello.elm
+++
@@ -1,7 +0,0 @@
-module Hello exposing (..)
-
-import Html exposing (..)
-import Element exposing (..)
-
-main =
-    toHtml <| show 42
