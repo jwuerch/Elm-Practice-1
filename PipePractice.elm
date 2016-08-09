1856f17cfd31750abf1e050ef33c8e602a9e9cbd
Jason Wuerch <wuerchjason@gmail.com>
2016-08-09 16:35:27 -0700

    add comments/code for display pipe practice when adding and multiplying elements together using multiple functions

--- PipePractice.elm
+++ PipePractice.elm
@@ -3,8 +3,27 @@ module PipePractice exposing (..)
 import Html exposing (..)
 import Element exposing (show)
 
+add : Int -> Int -> Int
+add x y =
+    x + y
+
+multiply : Int -> Int -> Int
+multiply x y =
+    x * y
+
+
+
 main =
     view
 
 view =
-    toHtml <| show 3
+    {- Here we use what's called the pipe operator.
+        It's an easier way to write multiple functions.
+        In this example, we take 3 and multiply it by 2
+        and then add 1 to that value. It can be a much cleaner way
+        of reading multiple functions. It's the same as the following:
+        add 1 (multiply 3 2) -}
+    Element.toHtml <| show (3
+                            |> multiply 2
+                            |> add 1
+                            )
