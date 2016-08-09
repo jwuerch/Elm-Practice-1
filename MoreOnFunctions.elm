0f706e2a20337606836feb6ed61b2d15438913c5
Jason Wuerch <wuerchjason@gmail.com>
2016-08-09 16:48:24 -0700

    create a function switch that takes in a tuple and switches the spots of a/b.

--- MoreOnFunctions.elm
+++ MoreOnFunctions.elm
@@ -3,10 +3,14 @@ module MoreOnFunctions exposing (..)
 import Html exposing (..)
 import Element exposing (show)
 
-
+{-Make your functions more generic when writing function signatures.
+This allows for more data types, unless you need to be really specific. -}
+switch : (a ,b) -> (b, a)
+switch (a, b) =
+    (b, a)
 
 main =
     view
 
 view =
-    Element.toHtml <| show "Hello"
+    Element.toHtml <| show (switch (3, 2))
